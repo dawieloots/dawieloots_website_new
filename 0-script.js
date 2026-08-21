73dc45def9f383af52d6cd9b7eef53f77edddb44
dawieloots-com <dawie@dawieloots.com>
2026-08-21 15:27:42 +0200

    added google analytics config

--- 0-script.js
+++
@@ -1,6 +0,0 @@
-// Small progressive-enhancement touch: mark external email action for analytics later.
-document.querySelectorAll('a[href^="mailto:"]').forEach(link => {
-  link.addEventListener('click', () => {
-    console.log('Contact link clicked');
-  });
-});
